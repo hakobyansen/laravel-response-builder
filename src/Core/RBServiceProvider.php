1f22ad20c44a6da1b3ec7669d38b7ace891a6318
Sen <sen.hakobyan@gmail.com>
2018-09-19 23:25:31 +0400

    small fix

--- src/Core/RBServiceProvider.php
+++ src/Core/RBServiceProvider.php
@@ -12,7 +12,7 @@ class RBServiceProvider extends ServiceProvider
 		// Publish configuration and RB_Request files
 		$this->publishes([
 			__DIR__.'/../Configs/config.php' => app()->basePath() . '/config/response_builder.php',
-			__DIR__ . '/../Requests/RB_Request.php' => app()->basePath() . '/app/Http/Requests/RB/RBRequest.php',
+			__DIR__ . '/../Requests/RB_Request.php' => app()->basePath() . '/app/Http/Requests/RB/RB_Request.php',
 		], 'laravel-response-builder');
 
 		// Register commands
