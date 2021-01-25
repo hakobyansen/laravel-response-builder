def790065d76609248841435d7f07acc6927e52f
Senik Hakobyan <sen.hakobyan@gmail.com>
2021-01-25 23:05:39 +0400

    Changed the default request_path and request_namespace

--- src/Configs/config.php
+++ src/Configs/config.php
@@ -6,13 +6,13 @@ return [
 	 * Generated request classes will be stored in specified "request_path" directory.
 	 */
 
-	'request_path' => base_path( 'app/Http/Requests/RB' ),
+	'request_path' => base_path( 'app/Http/Requests/ResponseBuilder' ),
 
 	/**
 	 * Namespace for generated request class.
 	 */
 
-	'request_namespace' => 'App\Http\Requests\RB',
+	'request_namespace' => 'App\Http\Requests\ResponseBuilder',
 
 	/**
 	 * Default value of Request's authorize() method.
